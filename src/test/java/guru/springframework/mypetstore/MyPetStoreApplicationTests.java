1053aa93bfa876b437b7d5de8a9975edc38a0b71
Rohit-Saha <35885539+Rohit-Saha@users.noreply.github.com>
2020-02-20 20:09:26 +0530

    First Commit

---
+++ src/test/java/guru/springframework/mypetstore/MyPetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mypetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
